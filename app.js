6881fdde96f02a82fe4f56ffa1ff37889444a4a8
MirzaAyon <ayonmirza98@gmail.com>
2022-06-19 22:28:11 +0600

    item name,price,quantity inputs and Add button added

--- app.js
+++ app.js
@@ -13,3 +13,23 @@ detailsButton.addEventListener('click', function ()
 })
 //ekhn buyer details e kichu likhe click korle sheta inspact er por console e show kore
 //text area shoho show krse
+const addProductBtn = document.getElementById('add-details-btn');
+
+addProductBtn.addEventListener('click', function () {
+    const itemName = document.getElementById('item-name-input');
+    const itemPrice = document.getElementById('item-price-input');
+    const itemQuantity = document.getElementById('item-quantity-input');
+
+    console.log(itemName.value, itemPrice.value, itemQuantity.value);
+    //ekhn ei 3 ta likhe add e click korle console e 3 ta show korbe
+    //ekhn upoer ja likha hoise ta nicher table e show korte hbe
+    //ekhn table take track krbo
+    // <tr>
+    //     <th >1</th>
+    //     <td>Mark</td>
+    //     <td>Otto</td>
+    //     <td>@mdo</td>
+    // </tr>
+    //etake js diye dynamic bhabe set krbo
+    //apatoto comment kore rakhsi
+})
